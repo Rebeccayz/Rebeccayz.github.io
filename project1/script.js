a0572a6b120b77b2e593d7f3c07c3e3e7da4de90
Rebecca <rebeccawangyzw@gmail.com>
2018-02-08 15:03:12 -0500

    adc

---
+++ project1/script.js
@@ -0,0 +1,20 @@
+var elements = document.getElementsByClassName('disappear')
+
+
+
+/*
+function drawRectangles(n){
+  for (i = 0; i < elements.length; i++){
+    var child = document.createElement("div"); //creating a new div
+    child.className = "element"; //assigning a class name to the div
+    parent.append(child); // appending the div to the element 'parent'
+  }
+}
+
+function Get(url){
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET",url,false);
+    Httpreq.send(null);
+    return Httpreq.responseText;
+}
+*/
