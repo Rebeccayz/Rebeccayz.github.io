6d84763f2a39f592af9f9b3a179bde4173ae0030
Rebecca <rebeccawangyzw@gmail.com>
2018-05-05 23:54:42 -0400

    1

--- finalproject/script.js
+++ finalproject/script.js
@@ -1,3 +1,4 @@
+
 function myFunction() {
     var x = document.getElementById("b");
     if (x.style.display === "none") {
@@ -5,4 +6,26 @@ function myFunction() {
     } else {
         x.style.display = "none";
     }
+}
+
+var current_image = document.getElementById('current_image');
+var button = document.getElementById('button');
+
+button.addEventListener('click', cyclePictures);
+
+var my_pictures = ['p0.png', 'p1.png', 'p2.png'];
+var picture_number = 0;
+
+function cyclePictures() {
+
+  if (picture_number === my_pictures.length) {
+    picture_number = 0;
+  }
+
+  current_image.src = 'p/' + my_pictures[picture_number];
+
+  console.log(my_pictures[picture_number]);
+
+  picture_number++;
+
 }
